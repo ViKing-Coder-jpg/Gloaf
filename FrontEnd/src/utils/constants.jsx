0dbc4e455ef4be80fbece4527379e11a6bc3a1e7
ViKing-Coder-jpg <vinaying342@gmail.com>
2026-03-28 14:45:27 +0530

    fix:server errors

--- FrontEnd/src/utils/constants.jsx
+++ FrontEnd/src/utils/constants.jsx
@@ -27,7 +27,7 @@ export const imageData = [
       "Spices not only add flavor but also offer antioxidant and digestive benefits.",
   },
 ];
-export const baseServerURL = "https://gloaf.onrender.com/api"
+export const baseServerURL = "/api"
 
 export const api = axios.create({
   baseURL: baseServerURL,
